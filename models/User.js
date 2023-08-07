fbd81fee29c333d71c478bcea8424b219908a6e0
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-08-07 20:02:21 +0100

    complete hw4

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, allowUpdateValidate } from "./hooks/index.js";
+import { emailRegexp } from "../constants/user-constants.js";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false }
+);
+
+userSchema.pre("findOneAndUpdate", allowUpdateValidate);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const User = model("users", userSchema);
+
+export default User;
---
+++ schemes/users-schema.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+import { emailRegexp } from "../constants/user-constants.js";
+
+const usersSignUpSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required().messages({
+    "any.required": `missing required email field`,
+  }),
+  password: Joi.string().min(6).required(),
+});
+const usersSignInSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required().messages({
+    "any.required": `missing required email field`,
+  }),
+  password: Joi.string().min(6).required(),
+});
+
+export default { usersSignUpSchema, usersSignInSchema };
